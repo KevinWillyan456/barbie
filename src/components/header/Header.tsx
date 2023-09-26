e12de80634dba40e113080a17efeb6c2071e12f8
Kevin Souza <kevinwillyansouza@gmail.com>
2023-09-26 16:55:40 -0400

    feat: adicionando responsividade

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,19 @@
+import { AiOutlineMenu } from "react-icons/ai";
 import ButtonForHeader from "./ButtonForHeader";
 import "./Header.css";
 import logo from "/barbie-logo.png";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+
   return (
     <header className="header">
       <div className="header-content">
-        <a href="/" className="logo">
+        <Link to="/" className="logo">
           <img src={logo} alt="React Logo" />
-        </a>
+        </Link>
         <ul className="navbar">
           <ButtonForHeader url="/">Ínicio</ButtonForHeader>
           <ButtonForHeader url="/">Loja</ButtonForHeader>
@@ -16,6 +21,25 @@ const Header = () => {
           <ButtonForHeader url="/">Blog</ButtonForHeader>
           <ButtonForHeader url="/">Sobre</ButtonForHeader>
         </ul>
+
+        {isOpenMenu && (
+          <ul className="navbar-mobile">
+            <ButtonForHeader url="/">Ínicio</ButtonForHeader>
+            <ButtonForHeader url="/">Loja</ButtonForHeader>
+            <ButtonForHeader url="/">Comunidade</ButtonForHeader>
+            <ButtonForHeader url="/">Blog</ButtonForHeader>
+            <ButtonForHeader url="/">Sobre</ButtonForHeader>
+          </ul>
+        )}
+
+        <div
+          className="hamburger-menu"
+          onClick={() => {
+            setIsOpenMenu(!isOpenMenu);
+          }}
+        >
+          <AiOutlineMenu />
+        </div>
       </div>
     </header>
   );
